a601d0e5e74a81d1606343172f3b3a49c21ce0e9
EdwinGarcia9 <eddwingarcia97@gmail.com>
2024-08-09 19:08:43 -0600

    make some changes

--- merge.py
+++ merge.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import geobr
-import geopandas as gpd
 
-def merge_dataframes(df_itens_pedidos, df_pedidos, df_productos, df_vendedores):
+def merge_dataframes(df_itens_pedidos, df_pedidos, df_productos, df_vendedores, coordenadas_brasil):
     """
     Realiza el merge entre los DataFrames de items de pedidos, pedidos, productos y vendedores,
     y añade la información geográfica de los estados brasileños.
@@ -31,20 +29,13 @@ def merge_dataframes(df_itens_pedidos, df_pedidos, df_productos, df_vendedores):
     df_merged = pd.merge(df_merged, df_vendedores, left_on='vendedor_id', right_on='vendedor_id_vend', how='left')
     df_merged.drop(columns=['vendedor_id_vend'], inplace=True)
 
-    # Cargar los datos de estados brasileños usando geobr
-    estados_brasil = geobr.read_state()
-
-    # Reproyectar las geometrías a un CRS geográfico (EPSG:4326)
-    estados_brasil = estados_brasil.to_crs(epsg=4326)
-
-    # Extraer latitud y longitud de la columna 'geometry'
-    estados_brasil['geolocation_lat'] = estados_brasil['geometry'].centroid.y
-    estados_brasil['geolocation_lng'] = estados_brasil['geometry'].centroid.x
-
-    # Filtrar la información de geobr para obtener las columnas necesarias
-    estados_brasil = estados_brasil[['abbrev_state', 'name_state', 'name_region', 'geolocation_lat', 'geolocation_lng']]
-
     # Realizar el merge con df_merged
-    df_merged = df_merged.merge(estados_brasil, on='abbrev_state', how='left')
+    df_merged = df_merged.merge(coordenadas_brasil, on='abbrev_state', how='left')
+    
+    # Eliminar la columna 'ciudad'
+    df_merged.drop(columns=['ciudad'], inplace=True)
+
+# Renombrar la columna 'valor_total' a 'valor_total_itens'
+    df_merged.rename(columns={'valor_total': 'valor_total_itens'}, inplace=True)
 
     return df_merged
